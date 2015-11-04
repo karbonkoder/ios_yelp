6bae29f7b22b60bdc91c877155f78661e79005c3
Ankush Raina <araina@groupon.com>
2015-11-03 17:28:35 -0800

    Added Search and Cancel to navigation controller for filters view

--- Yelp/FiltersViewController.m
+++ Yelp/FiltersViewController.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Cancel" style:UIBarButtonItemStylePlain target:self action:@selector(onCancelButton)];
+    
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Search" style:UIBarButtonItemStylePlain target:self action:@selector(onSearchButton)];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +28,16 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - Private methods
+
+- (void)onCancelButton {
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
-*/
+
+- (void)onSearchButton {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
 
 @end
